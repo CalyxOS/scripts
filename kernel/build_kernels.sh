dc3e6e956161d730ed3b91b6b39f24773ab3cba0
Chirayu Desai <chirayudesai1@gmail.com>
2022-01-05 06:27:47 +0530

    kernel: Add a script to build all kernels
    Change-Id: I0e4fcfdbef7a423c06b0153246494a06a749b7cf

---
+++ kernel/build_kernels.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# build_kernel:
+#
+#   Build Linux kernel for Android
+#
+#
+##############################################################################
+
+
+### SET ###
+
+# use bash strict mode
+set -euo pipefail
+
+
+### TRAPS ###
+
+# trap signals for clean exit
+trap 'exit $?' EXIT
+trap 'error_m interrupted!' SIGINT
+
+### CONSTANTS ###
+readonly script_path="$(cd "$(dirname "$(realpath "$0")")";pwd -P)"
+readonly vars_path="${script_path}/../vars/"
+readonly top="${script_path}/../../../"
+
+source "${vars_path}/kernels"
+
+## HELP MESSAGE (USAGE INFO)
+# TODO
+
+### FUNCTIONS ###
+build_kernels() {
+  pushd "${top}"
+  for kernel in ${kernels[@]}; do
+    ./build_kernel.sh "${kernel}"
+  done
+  popd
+}
+
+# error message
+# ARG1: error message for STDERR
+# ARG2: error status
+error_m() {
+  echo "ERROR: ${1:-'failed.'}" 1>&2
+  return "${2:-1}"
+}
+
+# print help message.
+help_message() {
+  echo "${help_message:-'No help available.'}"
+}
+
+main() {
+  build_kernels
+}
+
+### RUN PROGRAM ###
+
+main "${@}"
+
+##
