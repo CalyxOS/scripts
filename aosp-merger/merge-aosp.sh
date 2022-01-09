5ae8b34ad3e0f8ffec29036e5e851de6a8164383
Chirayu Desai <chirayudesai1@gmail.com>
2022-01-10 02:54:55 +0530

    aosp-merger: Add a tiny script that does the merging based on variables
    * Yes I know the name is confusing, suggestions welcome!
    
    Change-Id: I58bb25255293495bcb07667da1769f47640fdf4b

---
+++ aosp-merger/merge-aosp.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# merge-aosp:
+#
+#   Merge the latest AOSP release based on variables
+#
+#
+##############################################################################
+
+
+### SET ###
+
+# use bash strict mode
+set -euo pipefail
+
+
+### TRAPS ###
+
+# trap signals for clean exit
+trap 'exit $?' EXIT
+trap 'error_m interrupted!' SIGINT
+
+### CONSTANTS ###
+readonly script_path="$(cd "$(dirname "$0")";pwd -P)"
+readonly vars_path="${script_path}/../vars/"
+
+source "${vars_path}/common"
+
+## HELP MESSAGE (USAGE INFO)
+# TODO
+
+### FUNCTIONS ###
+
+run_aosp_merger() {
+  "${script_path}"/aosp-merger.sh merge "${prev_aosp_tag}" "${aosp_tag}"
+}
+
+squash_aosp_merge() {
+  "${script_path}"/squash.sh merge "${prev_aosp_tag}" "${aosp_tag}"
+}
+
+# error message
+# ARG1: error message for STDERR
+# ARG2: error status
+error_m() {
+  echo "ERROR: ${1:-'failed.'}" 1>&2
+  return "${2:-1}"
+}
+
+# print help message.
+help_message() {
+  echo "${help_message:-'No help available.'}"
+}
+
+main() {
+  run_aosp_merger
+  read -p "Waiting for conflict resolution before squashing. Press enter when done."
+  read -p "Once more, just to be safe"
+  squash_aosp_merge
+  echo "TODO: Implement gerrit upload"
+  echo "Don't forget to update the manifest!"
+}
+
+### RUN PROGRAM ###
+
+main "${@}"
+
+
+##
