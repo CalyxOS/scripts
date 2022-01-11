GrapheneOS-Archive/carriersettings-extractor

Prefer dedicated configs for duplicate carriers
Pixel Android 12 builds as of SPB5.210812.002 have a duplicate carrier
config (telenor_se), which has a dedicated config file but is also
included in others.pb. This causes the script to crash:

Traceback (most recent call last):
  File "./carriersettings_extractor.py", line 51, in <module>
    assert setting.canonicalName not in all_settings
AssertionError

Prefer dedicated configs in such cases to fix the issue.

carriersettings_extractor.py
@@ -41,7 +41,12 @@ for filename in glob(os.path.join(pb_path, '*.pb')):
             settings = MultiCarrierSettings()
             settings.ParseFromString(pb.read())
             for setting in settings.setting:
-                assert setting.canonicalName not in all_settings
+                if setting.canonicalName in all_settings:
+                    # Some carriers may have their own config files, as well as
+                    # a duplicate copy in others.pb. Prefer the dedicated
+                    # config if this is the case.
+                    continue
+
                 all_settings[setting.canonicalName] = setting
         else:
             setting = CarrierSettings()

CalyxOS/scripts

(cherry picked from commit 2ff7bb649d819e9e5973963b8d03d33a33a7b4db)
Change-Id: I34e32416d059570da285222f7a0a830e9b25bad6

carriersettings-extractor/carriersettings_extractor.py
@@ -93,12 +93,15 @@ def main():
                 settings = MultiCarrierSettings()
                 settings.ParseFromString(pb.read())
                 for setting in settings.setting:
-                    assert setting.canonical_name not in all_settings
+                    if setting.canonical_name in all_settings:
+                        # Some carriers may have their own config files, as well as
+                        # a duplicate copy in others.pb. Prefer the dedicated
+                        # config if this is the case.
+                        continue
                     all_settings[setting.canonical_name] = setting
             else:
                 setting = CarrierSettings()
                 setting.ParseFromString(pb.read())
-                assert setting.canonical_name not in all_settings
                 all_settings[setting.canonical_name] = setting
 
     carrier_config_root = ET.Element('carrier_config_list')