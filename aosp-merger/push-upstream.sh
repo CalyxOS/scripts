bbadb75ad63d40b954284abd49f4f648ae13ff43
Chirayu Desai <chirayudesai1@gmail.com>
2022-06-07 05:48:55 +0530

    aosp-merger: Get rid of squashing
    Change-Id: If5e4ff6672828c9af464e001224b02cd2e35782d

--- aosp-merger/push-upstream.sh
+++ aosp-merger/push-upstream.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 #
-# SPDX-FileCopyrightText: 2017, 2020-2022 The LineageOS Project
+# SPDX-FileCopyrightText: 2017, 2020-2021 The LineageOS Project
 # SPDX-FileCopyrightText: 2021-2022 The Calyx Institute
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 usage() {
-    echo "Usage ${0} <merge|rebase> <oldaosptag> <newaosptag>"
+    echo "Usage ${0} <newtag> <upstreambranch>"
 }
 
 # Verify argument count
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
     usage
     exit 1
 fi
 
-OPERATION="${1}"
-OLDTAG="${2}"
-NEWTAG="${3}"
-
-if [ "${OPERATION}" != "merge" -a "${OPERATION}" != "rebase" ]; then
-    usage
-    exit 1
-fi
+NEWTAG="${1}"
+UPSTREAMBRANCH="${2}"
 
 ### CONSTANTS ###
 readonly script_path="$(cd "$(dirname "$0")";pwd -P)"
@@ -32,13 +26,11 @@ readonly vars_path="${script_path}/../vars"
 source "${vars_path}/common"
 
 TOP="${script_path}/../../.."
-BRANCH="${calyxos_branch}"
-SQUASHBRANCH="squash/${BRANCH}_${OPERATION}-${NEWTAG}"
 
 # List of merged repos
 PROJECTPATHS=$(cat ${MERGEDREPOS} | grep -w merge | awk '{printf "%s\n", $2}')
 
-echo "#### Branch = ${BRANCH} Squash branch = ${SQUASHBRANCH} ####"
+echo "#### Branch = ${BRANCH} Upstream branch = ${UPSTREAMBRANCH} ####"
 
 # Make sure manifest and forked repos are in a consistent state
 echo "#### Verifying there are no uncommitted changes on CalyxOS forked AOSP projects ####"
@@ -54,7 +46,6 @@ echo "#### Verification complete - no uncommitted changes found ####"
 # Iterate over each forked project
 for PROJECTPATH in ${PROJECTPATHS}; do
     cd "${TOP}/${PROJECTPATH}"
-    echo "#### Pushing ${PROJECTPATH} squash to review ####"
-    git checkout "${SQUASHBRANCH}"
-    repo upload -c -y -o topic="${topic}" .
+    echo "#### Pushing upstream for ${PROJECTPATH} ####"
+    git push calyx ${NEWTAG}^{commit}:refs/heads/upstream/${UPSTREAMBRANCH}
 done
