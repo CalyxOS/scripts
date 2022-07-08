7d8b491e19c41842b53a441fd4f50452302f1633
Chirayu Desai <chirayudesai1@gmail.com>
2022-07-08 18:10:36 +0530

    New signing scripts
    Ref:
    * https://source.android.com/devices/tech/ota/sign_builds
    
    Change-Id: I5a2bbbe505c429eb7d37ed127e219e18025fa748

---
+++ sign/target-files.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2022 The Calyx Institute
+#
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# target-files:
+#
+#   Signs a target-files zip generated with `m target-files-package`
+#
+#
+##############################################################################
+
+
+### SET ###
+
+# use bash strict mode
+set -euo pipefail
+
+
+### TRAPS ###
+
+# trap signals for clean exit
+trap 'exit $?' EXIT
+trap 'error_m interrupted!' SIGINT
+
+### CONSTANTS ###
+readonly script_path="$(cd "$(dirname "$0")";pwd -P)"
+readonly vars_path="${script_path}/../vars"
+readonly top="${script_path}/../../.."
+
+readonly work_dir="${WORK_DIR:-$PWD}"
+
+source "${vars_path}/devices"
+
+readonly device="${1}"
+source "${vars_path}/${device}"
+
+## HELP MESSAGE (USAGE INFO)
+# TODO
+
+### FUNCTIONS ###
+
+# error message
+# ARG1: error message for STDERR
+# ARG2: error status
+error_m() {
+  echo "ERROR: ${1:-'failed.'}" 1>&2
+  return "${2:-1}"
+}
+
+# print help message.
+help_message() {
+  echo "${help_message:-'No help available.'}"
+}
+
+main() {
+  echo "Signing target-files"
+}
+
+### RUN PROGRAM ###
+
+main "${@}"
+
+
+##
