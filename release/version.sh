d8cc049a17ede8ce58884b95ebac0e2dd5c23235
Chirayu Desai <chirayudesai1@gmail.com>
2023-03-28 03:51:30 +0530

    Add script to print version
    Change-Id: Ieff76e69b98ad22608fba087d0835fa6fae1b344

---
+++ release/version.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2022 The Calyx Institute
+#
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# version:
+#
+#   Get build number from version
+#
+#
+##############################################################################
+
+
+### SET ###
+
+# use bash strict mode
+set -euo pipefail
+
+
+### TRAPS ###
+
+# trap signals for clean exit
+trap 'exit $?' EXIT
+trap 'error_m interrupted!' SIGINT
+
+### CONSTANTS ###
+readonly script_path="$(cd "$(dirname "$0")";pwd -P)"
+readonly top="${script_path}/../../.."
+readonly mk="${top}/vendor/calyx/config/version.mk"
+
+
+## HELP MESSAGE (USAGE INFO)
+# TODO
+
+### FUNCTIONS ###
+
+get_build_number() {
+  readonly year=$(date +%y)
+  readonly major=$(cat "${mk}" | grep ^PRODUCT_VERSION_MAJOR | awk '{printf "%s\n", $3}')
+  readonly minor=$(cat "${mk}" | grep ^PRODUCT_VERSION_MINOR | awk '{printf "%s\n", $3}')
+  readonly patch=$(cat "${mk}" | grep ^PRODUCT_VERSION_PATCH | awk '{printf "%s\n", $3}')
+  echo $((${year} * 1000000 + ${major} * 100000 +${minor} * 1000 + ${patch} * 10))
+}
+
+# error message
+# ARG1: error message for STDERR
+# ARG2: error status
+error_m() {
+  echo "ERROR: ${1:-'failed.'}" 1>&2
+  return "${2:-1}"
+}
+
+# print help message.
+help_message() {
+  echo "${help_message:-'No help available.'}"
+}
+
+main() {
+  get_build_number
+}
+
+### RUN PROGRAM ###
+
+main "${@}"
+
+
+##
+
