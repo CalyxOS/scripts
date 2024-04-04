9b79a0609532bd0b62492a234e3db34d77dc53df
Chirayu Desai <chirayudesai1@gmail.com>
2024-04-04 21:57:54 +0530

    Check in missing factory image scripts from 5.4.1
    Change-Id: Ibeefbddcaa3a4cd5a7ef678755463a05a1db859f

---
+++ factory/blueline/flash-all.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+:: Copyright 2012 The Android Open Source Project
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+::      http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing, software
+:: distributed under the License is distributed on an "AS IS" BASIS,
+:: WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: See the License for the specific language governing permissions and
+:: limitations under the License.
+
+PATH=%PATH%;"%SYSTEMROOT%\System32"
+fastboot getvar product 2>&1 | findstr /r /c:"^product: blueline" || echo "Factory image and device do not match. Please double check"
+fastboot getvar product 2>&1 | findstr /r /c:"^product: blueline" || exit /B 1
+fastboot flash bootloader bootloader-blueline-b1c1-0.4-7617406.img
+fastboot reboot-bootloader
+ping -n 5 127.0.0.1 >nul
+fastboot flash radio radio-blueline-g845-00194-210812-B-7635520.img
+fastboot reboot-bootloader
+ping -n 5 127.0.0.1 >nul
+fastboot erase avb_custom_key
+fastboot flash avb_custom_key avb_custom_key.img
+fastboot --skip-reboot -w update image-blueline-uq1a.240205.004.zip
+fastboot reboot-bootloader
+ping -n 5 127.0.0.1 >nul
+
+echo Press any key to exit...
+pause >nul
+exit
---
+++ factory/sargo/flash-all.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Copyright 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if ! [ $("$(which fastboot)" --version | grep "version" | cut -c18-23 | sed 's/\.//g' ) -ge 3301 ]; then
+  echo "fastboot too old; please download the latest version at https://developer.android.com/studio/releases/platform-tools.html"
+  exit 1
+fi
+fastboot getvar product 2>&1 | grep "^product: sargo$"
+if [ $? -ne 0 ]; then
+  echo "Factory image and device do not match. Please double check"
+  exit 1
+fi
+fastboot flash bootloader bootloader-sargo-b4s4-0.4-8048689.img
+fastboot reboot-bootloader
+sleep 5
+fastboot flash radio radio-sargo-g670-00145-220106-B-8048689.img
+fastboot reboot-bootloader
+sleep 5
+fastboot erase avb_custom_key
+fastboot flash avb_custom_key avb_custom_key.img
+fastboot --skip-reboot -w update image-sargo-uq1a.240205.004.zip
+fastboot reboot-bootloader
+sleep 5
