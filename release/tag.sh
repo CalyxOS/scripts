caf24e6e0107f202c8f39480d81c209f95ddad85
Chirayu Desai <chirayudesai1@gmail.com>
2024-12-27 22:25:37 +0530

    release: Speed up tag process, LFS
    * It's so, so much faster to push a LFS tag from a remote-tracking
      branch than then 'repo' default checked out state.
    * That holds even when the tag is identical to a previous tag,
      which is the case for many of our repos
    
    Test: (with an unpushed tag testLFStag, that is an existing commit on a branch)
      git checkout branch
      git tag -s testLFStag
      git push remote testLFStag # Takes a long time, interrupt it
      git checkout -b somebranch remote/branch
      git push remote testLFStag # Instant, pretty much
    
    Issue: https://github.com/git-lfs/git-lfs/issues/3976
    Change-Id: I1ddf299cc57ca042f95f1abe34b2dbd0211c81b8

--- release/tag.sh
+++ release/tag.sh
@@ -27,7 +27,10 @@ trap 'error_m interrupted!' SIGINT
 
 ### CONSTANTS ###
 readonly script_path="$(cd "$(dirname "$0")";pwd -P)"
+readonly vars_path="${script_path}/../vars"
 readonly top="${script_path}/../../.."
+
+source "${vars_path}/common"
 
 if [[ -e "${top}/build_kernel.sh" ]]; then
   readonly excluded_repos='CalyxOS/kernel_manifest'
@@ -66,7 +69,16 @@ tag_repo() {
 push_repo() {
   local repo="${1}"
   local version="${2}"
-  git -C "${repo}" push calyx "${version}"
+  pushd "${repo}"
+  # Fetch because this remote isn't fetched by default
+  git fetch calyx
+  # This should match the tag
+  # Pushing a LFS tag from a remote-tracking branch is MUCH, MUCH faster
+  # than the alternative
+  git checkout -b "lfs_${topic}" "calyx/${os_branch}" "${version}"
+  git push calyx "${version}"
+  git branch -D "lfs_${topic}"
+  popd
 }
 
 export -f push_repo
