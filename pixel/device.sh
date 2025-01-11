30b15caa43460fb2d33342e4e77dd06317d586f3
Michael Bestas <mkbestas@lineageos.org>
2025-01-11 06:21:14 +0200

    pixel: Use extract utils builtin download function
    Change-Id: I2a6fa8a566ca2599d31f052bdc6ee5b23306c909

--- pixel/device.sh
+++ pixel/device.sh
@@ -43,19 +43,17 @@ KEEP_DUMP=${KEEP_DUMP:-false}
 device() {
   local device="${1}"
   source "${vars_path}/${device}"
-  local factory_zip="${work_dir}/${device}/${build_id}/$(basename ${image_url})"
-  local extract_args="${factory_zip}"
-
-  "${script_path}/download.sh" "${device}"
+  local download_dir="${work_dir}/${device}/${build_id}"
+  local extract_args="--download-dir ${download_dir} --download-sha256 ${image_sha256} --regenerate"
 
   if [ "$KEEP_DUMP" == "true" ] || [ "$KEEP_DUMP" == "1" ]; then
     extract_args+=" --keep-dump"
   fi
 
-  extract_args+=" --extract-factory --regenerate"
+  extract_args+=" --extract-factory"
 
   pushd "${top}/device/google/${device}"
-  ./extract-files.py ${extract_args}
+  ./extract-files.py ${extract_args} ${image_url}
   popd
 
   echo "${build_id}" > "${top}/vendor/google/${device}/build_id.txt"
