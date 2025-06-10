0705b2556e9823f744c5f1fce827fb7bf99b48c3
Chirayu Desai <chirayudesai1@gmail.com>
2025-06-10 19:52:55 +0530

    New versioning scheme
    Issue: calyxos#3310
    Change-Id: Ie940709bc4c1b7856497320c7f309b3f18978ec3

--- release/version.sh
+++ release/version.sh
@@ -38,9 +38,10 @@ readonly mk="${top}/vendor/calyx/config/version.mk"
 get_build_number() {
   readonly year=$(date +%y)
   readonly major=$(cat "${mk}" | grep ^PRODUCT_VERSION_MAJOR | awk '{printf "%s\n", $3}')
-  readonly minor=$(cat "${mk}" | grep ^PRODUCT_VERSION_MINOR | awk '{printf "%s\n", $3}')
-  readonly patch=$(cat "${mk}" | grep ^PRODUCT_VERSION_PATCH | awk '{printf "%s\n", $3}')
-  version=$((${year} * 1000000 + ${major} * 100000 +${minor} * 1000 + ${patch} * 10))
+  readonly qpr=$(cat "${mk}" | grep ^PRODUCT_VERSION_QPR | awk '{printf "%s\n", $3}')
+  readonly month=$(cat "${mk}" | grep ^PRODUCT_VERSION_MONTH | awk '{printf "%s\n", $3}')
+  readonly release=$(cat "${mk}" | grep ^PRODUCT_VERSION_RELEASE | awk '{printf "%s\n", $3}')
+  version=$((${year} * 10000000 + ${major} * 100000 + ${qpr} * 10000 + ${month} * 10 + ${release}))
   echo "${version}"
 }
 
