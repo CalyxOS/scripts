f0770b56c8335be821201684f4bade9dbd86ccf9
Michael Bestas <mkbestas@gmail.com>
2025-07-27 14:13:38 +0300

    Update kernel build setup for 16
    Change-Id: Ia30dc1e8a827403a8f19cb9f0b79e368b0c1943c

--- kernel/build_kernel.sh
+++ kernel/build_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SPDX-FileCopyrightText: 2022 The Calyx Institute
+# SPDX-FileCopyrightText: 2022-2025 The Calyx Institute
 #
 # SPDX-License-Identifier: Apache-2.0
 
@@ -47,98 +47,21 @@ export OUT_DIR
 
 ### FUNCTIONS ###
 
-select_kernel_config() {
-  case ${kernel} in
-  crosshatch)
-    export BUILD_CONFIG=msm-4.9/private/msm-google/build.config.bluecross
-    export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
-    ;;
-  bonito)
-    export BUILD_CONFIG=msm-4.9/private/msm-google/build.config.bonito
-    export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
-    ;;
-  coral)
-    export BUILD_CONFIG=msm-4.14/private/msm-google/build.config.floral
-    export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
-    ;;
-  sunfish)
-    export BUILD_CONFIG=msm-4.14/private/msm-google/build.config.sunfish
-    export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
-    ;;
-  redbull)
-    export BUILD_CONFIG=redbull/private/msm-google/build.config.redbull.vintf
-    export KLEAF_SUPPRESS_BUILD_SH_DEPRECATION_WARNING=1
-    ;;
-  raviole)
-    export DEVICE_KERNEL_BUILD_CONFIG=gs201/private/gs-google/build.config.slider
-    export BUILD_AOSP_KERNEL=1
-    export LTO=full
-    ;;
-  bluejay)
-    export DEVICE_KERNEL_BUILD_CONFIG=gs201/private/devices/google/bluejay/build.config.bluejay
-    export BUILD_AOSP_KERNEL=1
-    export LTO=full
-    ;;
-  pantah)
-    export DEVICE_KERNEL_BUILD_CONFIG=gs201/private/gs-google/build.config.cloudripper
-    export BUILD_AOSP_KERNEL=1
-    export LTO=full
-    ;;
-  lynx)
-    export DEVICE_KERNEL_BUILD_CONFIG=gs201/private/devices/google/lynx/build.config.lynx
-    export BUILD_AOSP_KERNEL=1
-    export LTO=full
-    ;;
-  tangorpro)
-    export DEVICE_KERNEL_BUILD_CONFIG=gs201/private/devices/google/tangorpro/build.config.tangorpro
-    export BUILD_AOSP_KERNEL=1
-    export LTO=full
-    ;;
-  felix)
-    export DEVICE_KERNEL_BUILD_CONFIG=gs201/private/devices/google/felix/build.config.felix
-    export BUILD_AOSP_KERNEL=1
-    export LTO=full
-    ;;
-  *)
-    echo "Unsupported kernel ${kernel}"
-    echo "Support kernels: crosshatch bonito coral sunfish redbull raviole bluejay pantah lynx tangorpro felix"
-    exit
-    ;;
-  esac
-}
-
 build_kernel() {
   pushd "${top}"
-  # raviole/bluejay/pantah/lynx/tangorpro/felix is built differently, gki
-  if [[ "${kernel}" == "raviole" || "${kernel}" == "bluejay" || "${kernel}" == "pantah" || "${kernel}" == "lynx"
-      || "${kernel}" == "tangorpro" || "${kernel}" == "felix" ]]; then
-    gs201/private/gs-google/build_slider.sh "${@}"
-  else
-    build/build.sh "${@}"
-  fi
+  ./build_"${kernel}".sh
   popd
 }
 
 clean_kernel() {
-  if [[ "${kernel}" == "redbull" ]]; then
-    local dir="${top}/device/google/${kernel}-kernel/vintf/"
-  else
-    local dir="${top}/device/google/${kernel}-kernel/"
-  fi
+  local dir="${top}/device/google/${kernel}-kernels/calyx/"
   find "${dir}" -maxdepth 1 ! \( -name .gitreview -o -name .gitignore \) -type f -exec rm -f {} +
 }
 
 copy_kernel() {
-  # raviole/bluejay/pantah/lynx/tangorpro/felix is built differently, gki
-  if [[ "${kernel}" == "raviole" || "${kernel}" == "bluejay" || "${kernel}" == "pantah" || "${kernel}" == "lynx"
-      || "${kernel}" == "tangorpro" || "${kernel}" == "felix" ]]; then
-    cp -a "${OUT_DIR}/mixed/dist/"* "${top}/device/google/${kernel}-kernel/"
-  elif [[ "${kernel}" == "redbull" ]]; then
-    cp -a "${OUT_DIR}/dist/"* "${top}/device/google/${kernel}-kernel/vintf/"
-  else
-    cp -a "${OUT_DIR}/dist/"* "${top}/device/google/${kernel}-kernel/"
-  fi
-  echo " Files copied to ${top}/device/google/${kernel}-kernel/"
+  cp -a "${OUT_DIR}/dist/"* "${top}/device/google/${kernel}-kernels/calyx/"
+  chmod -x "${top}/device/google/${kernel}-kernels/calyx/*"
+  echo " Files copied to ${top}/device/google/${kernel}-kernels/calyx/"
 }
 
 # error message
@@ -155,7 +78,6 @@ help_message() {
 }
 
 main() {
-  select_kernel_config
   build_kernel "${@}"
   clean_kernel
   copy_kernel
